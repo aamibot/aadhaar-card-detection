811273814fdc3fb6e0537f193f39a09cd25cd53e
aamibot <ameerhussainkh@gmail.com>
2020-07-26 20:39:55 +0530

    removed unnecessary files

--- tool/utils.py
+++ tool/utils.py
@@ -1,63 +1,8 @@
 import cfg
-import sys
 import os
-import time
 import math
 import numpy as np
 import cv2
-import itertools
-import struct  # get_image_size
-import imghdr  # get_image_size
-
-
-def sigmoid(x):
-    return 1.0 / (np.exp(-x) + 1.)
-
-
-def softmax(x):
-    x = np.exp(x - np.expand_dims(np.max(x, axis=1), axis=1))
-    x = x / np.expand_dims(x.sum(axis=1), axis=1)
-    return x
-
-
-def bbox_iou(box1, box2, x1y1x2y2=True):
-    
-    # print('iou box1:', box1)
-    # print('iou box2:', box2)
-
-    if x1y1x2y2:
-        mx = min(box1[0], box2[0])
-        Mx = max(box1[2], box2[2])
-        my = min(box1[1], box2[1])
-        My = max(box1[3], box2[3])
-        w1 = box1[2] - box1[0]
-        h1 = box1[3] - box1[1]
-        w2 = box2[2] - box2[0]
-        h2 = box2[3] - box2[1]
-    else:
-        w1 = box1[2]
-        h1 = box1[3]
-        w2 = box2[2]
-        h2 = box2[3]
-
-        mx = min(box1[0], box2[0])
-        Mx = max(box1[0] + w1, box2[0] + w2)
-        my = min(box1[1], box2[1])
-        My = max(box1[1] + h1, box2[1] + h2)
-    uw = Mx - mx
-    uh = My - my
-    cw = w1 + w2 - uw
-    ch = h1 + h2 - uh
-    carea = 0
-    if cw <= 0 or ch <= 0:
-        return 0.0
-
-    area1 = w1 * h1
-    area2 = w2 * h2
-    carea = cw * ch
-    uarea = area1 + area2 - carea
-    return carea / uarea
-
 
 def nms_cpu(boxes, confs, nms_thresh=0.5, min_mode=False):
     # print(boxes.shape)
@@ -146,18 +91,6 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
         cv2.imwrite(os.path.join(cfg.DETECTION_FOLDER,savename), img)
     return img
 
-
-def read_truths(lab_path):
-    if not os.path.exists(lab_path):
-        return np.array([])
-    if os.path.getsize(lab_path):
-        truths = np.loadtxt(lab_path)
-        truths = truths.reshape(truths.size / 5, 5)  # to avoid single truth problem
-        return truths
-    else:
-        return np.array([])
-
-
 def load_class_names(namesfile):
     class_names = []
     with open(namesfile, 'r') as fp:
@@ -166,8 +99,6 @@ def load_class_names(namesfile):
         line = line.rstrip()
         class_names.append(line)
     return class_names
-
-
 
 def post_processing(img, conf_thresh, nms_thresh, output):
 
